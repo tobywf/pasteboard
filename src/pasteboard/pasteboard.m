15afc43f61da52378cc8d0f367030affd4efaf72
Toby Fleming <tobywf@users.noreply.github.com>
2021-02-04 20:33:19 -0800

    Clang format

--- src/pasteboard/pasteboard.m
+++ src/pasteboard/pasteboard.m
@@ -1,10 +1,10 @@
 /*
-    Pasteboard - Python interface for reading from NSPasteboard (macOS clipboard)
-    Copyright (C) 2017-2021  Toby Fleming
+Pasteboard - Python interface for reading from NSPasteboard (macOS clipboard)
+Copyright (C) 2017-2021  Toby Fleming
 
-    This Source Code Form is subject to the terms of the Mozilla Public License,
-    v. 2.0. If a copy of the MPL was not distributed with this file, You can
-    obtain one at https://mozilla.org/MPL/2.0/.
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at https://mozilla.org/MPL/2.0/.
 */
 #include <AppKit/AppKit.h>
 #define PY_SSIZE_T_CLEAN
@@ -15,10 +15,10 @@
 // it's not really a use-case I anticipate being useful for clipboard access...
 static NSAutoreleasePool *pool = NULL;
 
-typedef NSString * NSPasteboardType;
+typedef NSString *NSPasteboardType;
 static PyObject *PasteboardType_Default = NULL;
 
-typedef enum {DATA, STRING} PasteboardTypeReading;
+typedef enum { DATA, STRING } PasteboardTypeReading;
 typedef struct {
     PyObject_HEAD
     NSPasteboardType type;
@@ -34,11 +34,13 @@ pasteboardtype_repr(PyObject *self)
 }
 
 PyDoc_STRVAR(pasteboardtype__doc__,
-"Internal type to expose NSPasteboardTypes to Python.");
+             "Internal type to expose NSPasteboardTypes to Python.");
 
 PyTypeObject PasteboardTypeType = {
+    // clang-format off
     PyVarObject_HEAD_INIT(&PyType_Type, 0)
     .tp_name = "pasteboard.PasteboardType",
+    // clang-format on
     .tp_doc = pasteboardtype__doc__,
     .tp_basicsize = sizeof(PasteboardTypeState),
     .tp_itemsize = 0,
@@ -68,7 +70,9 @@ typedef struct {
 } PasteboardState;
 
 static PyObject *
-pasteboard_new(PyTypeObject *type, PyObject *Py_UNUSED(args), PyObject *Py_UNUSED(kwargs))
+pasteboard_new(PyTypeObject *type,
+               PyObject *Py_UNUSED(args),
+               PyObject *Py_UNUSED(kwargs))
 {
     PasteboardState *state = (PasteboardState *)type->tp_alloc(type, 0);
     if (!state) {
@@ -102,30 +106,30 @@ pasteboard_repr(PyObject *self)
 }
 
 static PyObject *
-get_contents(NSPasteboard *board, PasteboardTypeState* type)
+get_contents(NSPasteboard *board, PasteboardTypeState *type)
 {
     switch (type->read) {
-        case DATA: {
-            NSData *data = [board dataForType:type->type];
-            if (!data) {
-                Py_RETURN_NONE;
-            }
-
-            return PyBytes_FromStringAndSize([data bytes], [data length]);
+    case DATA: {
+        NSData *data = [board dataForType:type->type];
+        if (!data) {
+            Py_RETURN_NONE;
         }
 
-        case STRING: {
-            NSString *str = [board stringForType:type->type];
-            if (!str) {
-                Py_RETURN_NONE;
-            }
+        return PyBytes_FromStringAndSize([data bytes], [data length]);
+    }
 
-            return PyUnicode_FromString([str UTF8String]);
+    case STRING: {
+        NSString *str = [board stringForType:type->type];
+        if (!str) {
+            Py_RETURN_NONE;
         }
 
-        default:
-            PyErr_SetString(PyExc_RuntimeError, "Unknown pasteboard type");
-            return NULL;
+        return PyUnicode_FromString([str UTF8String]);
+    }
+
+    default:
+        PyErr_SetString(PyExc_RuntimeError, "Unknown pasteboard type");
+        return NULL;
     }
 }
 
@@ -140,8 +144,7 @@ pasteboard_get_contents(PyObject *self, PyObject *args, PyObject *kwargs)
     static char *kwlist[] = {"type", "diff", NULL};
 
     if (!PyArg_ParseTupleAndKeywords(
-            args, kwargs, "|O!p", kwlist,
-            &PasteboardTypeType, &type, &diff)) {
+            args, kwargs, "|O!p", kwlist, &PasteboardTypeType, &type, &diff)) {
         return NULL;
     }
 
@@ -156,63 +159,65 @@ pasteboard_get_contents(PyObject *self, PyObject *args, PyObject *kwargs)
     return get_contents(state->board, (PasteboardTypeState *)type);
 }
 
-PyDoc_STRVAR(pasteboard_get_contents__doc__,
-"get_contents(type: PasteboardType = String, diff: bool = False) -> Union[str, bytes, None]\n\n"
-"Gets the contents of the pasteboard.\n\n"
-"type - The NSPasteboardType to get, see module members. Default is 'String'.\n"
-"diff - Only get the contents if it has changed. Otherwise, `None` is returned. "
-"Can be used for efficiently querying the pasteboard when polling for changes."
-"Default is `False`.\n\n"
-"Returns `str` for string types (text, HTML, RTF, etc.), and "
-"`bytes` for binary types (PDF, PNG, TIFF, etc.). `None` is returned "
-"if an error occurred, there is no data of the requested type, "
-"or `diff` was set to `True` and the contents has not changed since "
-"the last query.");
+PyDoc_STRVAR(
+    pasteboard_get_contents__doc__,
+    "get_contents(type: PasteboardType = String, diff: bool = False) -> Union[str, bytes, None]\n\n"
+    "Gets the contents of the pasteboard.\n\n"
+    "type - The NSPasteboardType to get, see module members. Default is 'String'.\n"
+    "diff - Only get the contents if it has changed. Otherwise, `None` is returned. "
+    "Can be used for efficiently querying the pasteboard when polling for changes."
+    "Default is `False`.\n\n"
+    "Returns `str` for string types (text, HTML, RTF, etc.), and "
+    "`bytes` for binary types (PDF, PNG, TIFF, etc.). `None` is returned "
+    "if an error occurred, there is no data of the requested type, "
+    "or `diff` was set to `True` and the contents has not changed since "
+    "the last query.");
 
 static PyObject *
-set_contents(NSPasteboard *board, PasteboardTypeState* type, const char *bytes, Py_ssize_t length)
+set_contents(NSPasteboard *board,
+             PasteboardTypeState *type,
+             const char *bytes,
+             Py_ssize_t length)
 {
     switch (type->read) {
-        case DATA: {
-            NSData* data = [NSData
-                dataWithBytesNoCopy:(void *)bytes
-                length:length
-                freeWhenDone:FALSE];
-            if (!data) {
-                PyErr_SetString(PyExc_RuntimeError, "Failed to allocate data");
-                return NULL;
-            }
-
-            [board clearContents];
-            if ([board setData:data forType:type->type]) {
-                Py_RETURN_TRUE;
-            } else {
-                Py_RETURN_FALSE;
-            }
-        }
-
-        case STRING: {
-            NSString *str = [[NSString alloc]
-                initWithBytesNoCopy:(void *)bytes
-                length:length
-                encoding:NSUTF8StringEncoding
-                freeWhenDone:FALSE];
-            if (!str) {
-                PyErr_SetString(PyExc_RuntimeError, "Failed to allocate string");
-                return NULL;
-            }
-
-            [board clearContents];
-            if ([board setString:str forType:type->type]) {
-                Py_RETURN_TRUE;
-            } else {
-                Py_RETURN_FALSE;
-            }
-        }
-
-        default:
-            PyErr_SetString(PyExc_RuntimeError, "Unknown pasteboard type");
+    case DATA: {
+        NSData *data = [NSData dataWithBytesNoCopy:(void *)bytes
+                                            length:length
+                                      freeWhenDone:FALSE];
+        if (!data) {
+            PyErr_SetString(PyExc_RuntimeError, "Failed to allocate data");
             return NULL;
+        }
+
+        [board clearContents];
+        if ([board setData:data forType:type->type]) {
+            Py_RETURN_TRUE;
+        } else {
+            Py_RETURN_FALSE;
+        }
+    }
+
+    case STRING: {
+        NSString *str = [[NSString alloc] initWithBytesNoCopy:(void *)bytes
+                                                       length:length
+                                                     encoding:NSUTF8StringEncoding
+                                                 freeWhenDone:FALSE];
+        if (!str) {
+            PyErr_SetString(PyExc_RuntimeError, "Failed to allocate string");
+            return NULL;
+        }
+
+        [board clearContents];
+        if ([board setString:str forType:type->type]) {
+            Py_RETURN_TRUE;
+        } else {
+            Py_RETURN_FALSE;
+        }
+    }
+
+    default:
+        PyErr_SetString(PyExc_RuntimeError, "Unknown pasteboard type");
+        return NULL;
     }
 }
 
@@ -227,22 +232,28 @@ pasteboard_set_contents(PyObject *self, PyObject *args, PyObject *kwargs)
 
     static char *kwlist[] = {"data", "type", NULL};
 
-    if (!PyArg_ParseTupleAndKeywords(
-            args, kwargs, "s#|O!", kwlist,
-            &bytes, &length, &PasteboardTypeType, &type)) {
+    if (!PyArg_ParseTupleAndKeywords(args,
+                                     kwargs,
+                                     "s#|O!",
+                                     kwlist,
+                                     &bytes,
+                                     &length,
+                                     &PasteboardTypeType,
+                                     &type)) {
         return NULL;
     }
 
     return set_contents(state->board, (PasteboardTypeState *)type, bytes, length);
 }
 
-PyDoc_STRVAR(pasteboard_set_contents__doc__,
-"set_contents(data: Union[str, bytes], type: PasteboardType = String) -> bool\n\n"
-"Sets the contents of the pasteboard.\n\n"
-"data - str or bytes-like object. If type is a string type and bytes is not "
-"UTF-8 encoded, the behaviour is undefined.\n"
-"type - The NSPasteboardType to get, see module members. Default is 'String'.\n"
-"Returns `True` if the operation was successful; otherwise, `False`.");
+PyDoc_STRVAR(
+    pasteboard_set_contents__doc__,
+    "set_contents(data: Union[str, bytes], type: PasteboardType = String) -> bool\n\n"
+    "Sets the contents of the pasteboard.\n\n"
+    "data - str or bytes-like object. If type is a string type and bytes is not "
+    "UTF-8 encoded, the behaviour is undefined.\n"
+    "type - The NSPasteboardType to get, see module members. Default is 'String'.\n"
+    "Returns `True` if the operation was successful; otherwise, `False`.");
 
 static PyObject *
 pasteboard_get_file_urls(PyObject *self, PyObject *args, PyObject *kwargs)
@@ -271,8 +282,9 @@ pasteboard_get_file_urls(PyObject *self, PyObject *args, PyObject *kwargs)
     }
 
     NSArray<Class> *classes = @[[NSURL class]];
-    NSDictionary *options = @{};
-    NSArray<NSURL*> *files = [state->board readObjectsForClasses:classes options:options];
+    NSDictionary *options = @ {};
+    NSArray<NSURL *> *files = [state->board readObjectsForClasses:classes
+                                                          options:options];
 
     Py_ssize_t len = (Py_ssize_t)[files count];
     PyObject *urls = PyTuple_New(len);
@@ -287,23 +299,25 @@ pasteboard_get_file_urls(PyObject *self, PyObject *args, PyObject *kwargs)
 
     if (len != pos) {
         if (_PyTuple_Resize(&urls, pos) != 0) {
-            PyErr_SetString(PyExc_RuntimeError, "Internal error: failed to resize tuple");
+            PyErr_SetString(PyExc_RuntimeError,
+                            "Internal error: failed to resize tuple");
             return NULL;
         }
     }
     return urls;
 }
 
-PyDoc_STRVAR(pasteboard_get_file_urls__doc__,
-"get_file_urls(diff: bool = False) -> Optional[Sequence[str]]\n\n"
-"Gets the contents of the pasteboard as file URLs.\n\n"
-"diff - Only get the contents if it has changed. Otherwise, `None` is returned. "
-"Can be used for efficiently querying the pasteboard when polling for changes."
-"Default is `False`.\n\n"
-"Returns a sequence of strings corresponding to the file URL's path. "
-"`None` is returned if an error occurred, there is no data of the requested "
-"type, or `diff` was set to `True` and the contents has not changed since "
-"the last query.");
+PyDoc_STRVAR(
+    pasteboard_get_file_urls__doc__,
+    "get_file_urls(diff: bool = False) -> Optional[Sequence[str]]\n\n"
+    "Gets the contents of the pasteboard as file URLs.\n\n"
+    "diff - Only get the contents if it has changed. Otherwise, `None` is returned. "
+    "Can be used for efficiently querying the pasteboard when polling for changes."
+    "Default is `False`.\n\n"
+    "Returns a sequence of strings corresponding to the file URL's path. "
+    "`None` is returned if an error occurred, there is no data of the requested "
+    "type, or `diff` was set to `True` and the contents has not changed since "
+    "the last query.");
 
 static PyMethodDef pasteboard_methods[] = {
     {
@@ -324,15 +338,17 @@ static PyMethodDef pasteboard_methods[] = {
         METH_VARARGS | METH_KEYWORDS,
         pasteboard_get_file_urls__doc__,
     },
-    {NULL, NULL, 0, NULL}  /* Sentinel */
+    {NULL, NULL, 0, NULL} /* Sentinel */
 };
 
 PyDoc_STRVAR(pasteboard__doc__,
-"Holds a reference to the global pasteboard for reading and writing.");
+             "Holds a reference to the global pasteboard for reading and writing.");
 
 PyTypeObject PasteboardType = {
+    // clang-format off
     PyVarObject_HEAD_INIT(&PyType_Type, 0)
     .tp_name = "pasteboard.Pasteboard",
+    // clang-format on
     .tp_doc = pasteboard__doc__,
     .tp_basicsize = sizeof(PasteboardState),
     .tp_itemsize = 0,
@@ -349,19 +365,19 @@ module_free(void *);
 PyDoc_STRVAR(module__doc__, "Python interface for NSPasteboard (macOS clipboard)");
 
 static struct PyModuleDef pasteboard_module = {
-   PyModuleDef_HEAD_INIT,
-   .m_name = "_native",
-   .m_doc = module__doc__,
-   .m_size = -1,
-   .m_free = module_free,
+    PyModuleDef_HEAD_INIT,
+    .m_name = "_native",
+    .m_doc = module__doc__,
+    .m_size = -1,
+    .m_free = module_free,
 };
 
 #define QUOTE(str) #str
-#define PASTEBOARD_TYPE(name, read)  \
-    PyObject *__##name = pasteboardtype_new(NSPasteboardType##name, read); \
-    Py_INCREF(__##name); \
-    if (PyModule_AddObject(module, QUOTE(name), __##name) < 0) {  \
-        goto except;  \
+#define PASTEBOARD_TYPE(name, read)                                                    \
+    PyObject *__##name = pasteboardtype_new(NSPasteboardType##name, read);             \
+    Py_INCREF(__##name);                                                               \
+    if (PyModule_AddObject(module, QUOTE(name), __##name) < 0) {                       \
+        goto except;                                                                   \
     }
 
 PyMODINIT_FUNC
