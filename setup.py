e76f419288ff22550fe2c09884688da1e3a018a9
Fangchen Li <fangchen.li@outlook.com>
2024-01-13 16:48:09 -0800

    set min macos version to 10.13, add github release workflow

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from setuptools import setup, Extension
 pasteboard = Extension(
     "pasteboard._native",
     ["src/pasteboard/pasteboard.m"],
-    extra_compile_args=["-Wall", "-Wextra", "-Wpedantic", "-Werror"],
+    extra_compile_args=[
+        "-mmacosx-version-min=10.13",
+        "-Wall",
+        "-Wextra",
+        "-Wpedantic",
+        "-Werror",
+    ],
     extra_link_args=["-framework", "AppKit"],
     language="objective-c",
 )
